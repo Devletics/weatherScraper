75ae13a939a0452fbb188dd51b1e2840b8f09889
Joshua Fluke <Jfluke717@gmail.com>
2016-06-01 17:06:10 -0600

    scrape
    simple scrape

--- scraper.php
+++ scraper.php
@@ -1,10 +1,9 @@
 <?php
 
-echo
 $contents = file_get_contents("http://www.weather-forecast.com/locations/Salt-Lake-City/forecasts/latest");
 
-preg_match('/3 Day Weather Forecast Summary: <\/b> <span class="phrase">(.*?)</s', $contents, $matches);
-
+preg_match("/3 Day Weather Forecast Summary:/i", $contents, $matches); //this is extracting our piece of information
+echo '<pre>';
 print_r($matches);
-
+echo '</pre>';
  ?>
