8bf44f063704e9aa035513bd230aa6c391b41bd1
Joshua Fluke <Jfluke717@gmail.com>
2016-06-02 10:29:27 -0600

    search function
    search city

--- scraper.php
+++ scraper.php
@@ -1,9 +1,16 @@
 <?php
 
+
+$city=$_GET['city'];
+
+$city=str_replace(" ", "", $city);
+
 $contents = file_get_contents("http://www.weather-forecast.com/locations/Salt-Lake-City/forecasts/latest");
 
-preg_match("/3 Day Weather Forecast Summary:/i", $contents, $matches); //this is extracting our piece of information
-echo '<pre>';
-print_r($matches);
-echo '</pre>';
- ?>
+preg_match('/3 Day Weather Forecast Summary:<\/b><span class="phrase">(/*?)</s', $contents, $matches); //this is extracting our piece of information
+
+
+echo $matches[1];
+
+
+?>
